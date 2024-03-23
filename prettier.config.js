71580790f1209c508c817783e081b5713b9c1e6e
Morgan Spencer <morgan@spencercreative.co>
2024-03-22 22:53:49 -0400

    Setup root repo

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/** @type {import('prettier').RequiredOptions} */
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  editorconfig: true,
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  plugins: ['prettier-plugin-tailwindcss'],
+}
